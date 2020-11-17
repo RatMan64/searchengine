de4ae1a2fc6dcafe6b7f2f31699faed78a423fbc
RileyRat5x <35906066+RileyRat5x@users.noreply.github.com>
2020-11-16 16:15:49 -0800

    added two funtions
    added first page and grabitems

--- scraper.py
+++ scraper.py
@@ -1,22 +1,75 @@
-#RileyBarnes Kelly
-#cs454
+# RileyBarnes Kelly
+# cs454
 
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
-#get dataset from the-house.com
 
-#begining url
+# get dataset from the-house.com
+
+# begining url
 url = "https://www.the-house.com/boardshop.html"
-uClient = uReq(url)
 
-soup =bs(uClient.read(), "html.parser")
-uClient.close()
-#scrape for snowboards, snowbard jackets, snowboard pants, gloves, beanies, base layers, snowboard boots, bidings, helmets, goggles, bags.
-# feilds = item desc, type, brand,  year, size, price, url, is onsale, gender
 
-headers = "itemDescription, Type, Brand, Year, Size, Price, Url, Onsale, Gender"
-out_filename = "SearchEngineData.csv"
+regUrl = "www.The-House.com"
 
-file = open(out_filename,"w", encoding= 'utf-8' )
-file.write(headers)
 
+def firstPage():
+    uClient = uReq(url)
+
+    soup = bs(uClient.read(), "html.parser")
+    uClient.close()
+    # scrape for snowboards, snowbard jackets, snowboard pants, gloves, beanies, base layers, snowboard boots, bidings, helmets, goggles, bags.
+    # feilds = item desc, picture, type, brand,  year, size, price, url, is onsale, gender
+
+    headers = "itemDescription, picture,  Type, Brand, Year, Size, Price, Url, Onsale, Gender"
+    out_filename = "SearchEngineData.csv"
+
+    file = open(out_filename, "w", encoding='utf-8')
+    file.write(headers)
+
+
+    for catigory in catigory:
+        
+
+
+
+
+
+
+
+
+def grabItems(extentionUrl, itemType):
+    uClient = uReq(regUrl+extentionUrl)
+    cagigorysoup = bs(uClient.read(), "html.parser")
+    uClient.close()
+    itemDesc = ""
+    picture = ""
+    brand = ""
+    year=""
+
+    return
+
+# snowBoards
+
+
+# jackets
+
+
+# pants
+
+# gloves
+
+# beanies
+
+# base layers
+
+# snowboard boots
+
+# bindings
+
+
+# helmet
+
+# goggles
+
+# bags
