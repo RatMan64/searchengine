d76fb2191bf5d750e8b02791b8bfe36ce508c434
RileyRat5x <35906066+RileyRat5x@users.noreply.github.com>
2020-11-24 19:25:41 -0800

    updated the scraper should work
    scraper works for snowboards at this point ill run it for the night untill it hits a problem otherwise i ll commit the data soon

--- scraper.py
+++ scraper.py
@@ -11,8 +11,8 @@ import time
 url = "https://www.the-house.com/boardshop.html"
 
 # url extenstion
-regUrl = "www.The-House.com"
-headers = "itemDescription, picture,  Type, Brand, Year, Size, Price, Url, Onsale, Gender"
+regUrl = "https://www.The-House.com"
+headers = "itemName,itemDescription, picture,  Type, Brand, Year, Size, Price, Url, Onsale, discountr"
 out_filename = "SearchEngineData.csv"
 
 file = open(out_filename, "w", encoding='utf-8')
@@ -29,54 +29,128 @@ def beginScrape():
     # feilds = item desc, picture, type, brand,  year, size, price, url, is onsale, gender
 
     for catigory in catigorys:
-        catigoryDetails = catigory.find("div", {"class": "category-name"})
-        catigoryName = catigoryDetails.span.name
+        # print(catigory)
+        catigoryName = catigory.a.span["name"]
+        # print(catigoryName)
+        # print(type(catigoryName))
+
+        urlExt = catigory.a["href"]
+        # print(urlExt)
 
         # skip snow skaters
         if catigoryName == "Snow Skate, Snow Surfers":
             continue
-        # if its a catigory that has sub classes add that to the grab items
-        if catigoryName == "SnowBoarding Clothes & Apparel":
-            subCats()
+        # if its a catigory that has sub classes go to the subcats function
+        if catigoryName == "SnowBoarding Clothes & Apparel" or catigoryName =="Snowboard Accessories" or catigoryName =="Splitboard Gear":
+            subCats(urlExt)
         else:
-            grabItems()
+            grabItems(urlExt,catigoryName)
 
     file.close()
-
-
+#TODO! finish subcats
+# sub cats is basically thefirst for loop but needs to be done as some catigorys have more catigorys
 def subCats(extentionUrl):
+    print("Sub Cats Entered")
     subClient = uReq(regUrl+extentionUrl)
     subsoup = bs(subClient.read(), "html.parser")
     subClient.close()
     time.sleep(5)
     cats = subsoup.findAll("div", {"class": "col-xs-6 skinny"})
     for cat in cats:
+        catDetails = cat.find("div", {"class": "category-name"})
+        catName = catDetails.span["name"]
+        #Clothing accessories has more categorys
+        if catName == "Clothing Accessories":
+            subCats(catDetails.a["name"])
+        grabItems(catDetails.a["href"],catName)
 
     return
 
 
 def grabItems(extentionUrl, itemType):
+    print("grab Items entered")
     # first we look at the first page of the catigory we change the url as we have completed the page
     newClient = uReq(regUrl + extentionUrl)
     cagigorysoup = bs(newClient.read(), "html.parser")
     newClient.close()
     time.sleep(5)
     Type = itemType
-
-    # while loop that writes each catigroy item to the file
-
-    for item in items:
-        itemDesc = ""
-        picture = ""
-        brand = ""
-        year = ""
-        size = ""
-        price = ""
-        url = ""
-        onsale = ""
-        Gender = ""
-
+    items = cagigorysoup.findAll("div", {"class": "col-sm-3 col-xs-6 col-xs-print-3 pull-left"})
+    grab(Type, items, regUrl+extentionUrl, cagigorysoup)
     return
+
+def grab(Type, items,url, oldsoup ):
+    count =1
+    endofitems = oldsoup.findAll("a", {"aria-label": "Go to last page"})
+    # print(endofitems)
+    # print(endofitems[0]["href"])
+    while ("?nocache=1&refine.pagenumber=" + str(count)) != endofitems[0]["href"]:
+        if count>1:
+            page ="?nocache=1&refine.pagenumber="+str(count)
+            newUrl = url+page
+            newPageClient = uReq(newUrl)
+            soup = bs(newPageClient.read(), "html.parser")
+            newPageClient.close()
+            time.sleep(5)
+            items = soup.findAll("div", {"class": "col-sm-3 col-xs-6 col-xs-print-3 pull-left"})
+        # print(len(items))
+        # print(count)
+        for item in items:
+
+            onsale = "F"
+            discount = ""
+            name = item.div.div.a.img["alt"]
+            picture = item.div.div.a.img["src"]
+            brandtitle = item.findAll("meta",{"itemprop": "brand"})
+            # print(brandtitle[0]["content"])
+            brand = brandtitle[0]["content"]
+            # print(repr(brand))
+            # print(repr(picture))
+            # print(repr(name))
+            #year = ""
+            sizes =item.findAll("div",{"class": "category-product-title sizelist clearfix"})
+            # print(sizes)
+            size = sizes[0].text
+
+            size = size.replace("Size:","").replace(",","-").replace("\n","").replace("\t","").strip()
+            # print(type(size))
+            # print(repr(size))
+
+
+            prePrice = item.findAll("span", {"itemprop": "price"})
+            price = prePrice[0].text
+            preurl = item.div.findAll("div", {"class": "category-product-title"})
+            itemUrl = regUrl + preurl[0].a["href"]
+            # print(itemUrl)
+            pageClient= uReq(itemUrl)
+            pagehtml = bs(pageClient.read(), "html.parser")
+            time.sleep(5)
+            pageClient.close()
+            predesc =pagehtml.findAll("div", {"class": "col-sm-8 col-xs-12"})
+            # print(predesc[0].div.p)
+            itemDesc = predesc[0].div.p.text
+            # itemDesc= str(itemDesc).strip("\\t\\n")
+            itemDesc = itemDesc.strip()
+            itemDesc = itemDesc.replace("\n", "")
+            itemDesc = itemDesc.replace(",","")
+
+
+            #print(itemDesc)
+
+            dis=item.findAll("span",{"class":"category-product-discount sale"})
+            # print(dis)
+            if len(dis) != 0:
+                onsale = "T"
+                discount = str(dis[0].text)
+                discount = discount.replace("\t","").replace("\n","")
+                # print(repr(discount))
+            else:
+                discount=""
+            # feilds = name,item desc, picture, type, brand,  year, size, price, url, is onsale, gender
+            #print(repr(str(name)+","+str(itemDesc) + ","+ str(picture)+ ","+ str(Type)+","+str(brand)+ ","+ str(size)+ ","+ str(price)+ ","+ str(itemUrl)+"," + onsale +"," + str(discount)))
+            file.write(str(name)+","+str(itemDesc) + ","+ str(picture)+ ","+ str(Type)+","+str(brand)+ ","+ str(size)+ ","+ str(price)+ ","+ str(itemUrl)+"," + str(onsale)+"," + str(discount)+ "\n" )
+        count +=1
+
 
 
 def main():
@@ -85,27 +159,3 @@ def main():
 
 main()
 
-# snowBoards
-
-
-# jackets
-
-
-# pants
-
-# gloves
-
-# beanies
-
-# base layers
-
-# snowboard boots
-
-# bindings
-
-
-# helmet
-
-# goggles
-
-# bags
