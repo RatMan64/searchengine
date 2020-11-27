a77c85da7e8cd17ab3bfeefb8868fcabc456319b
RileyRat5x <35906066+RileyRat5x@users.noreply.github.com>
2020-11-26 18:18:34 -0800

    scraper completed and scrap is done

--- scraper.py
+++ scraper.py
@@ -12,7 +12,7 @@ url = "https://www.the-house.com/boardshop.html"
 
 # url extenstion
 regUrl = "https://www.The-House.com"
-headers = "itemName,itemDescription, picture,  Type, Brand, Year, Size, Price, Url, Onsale, discountr"
+headers = "itemName,itemDescription, picture,  Type, Brand, Size, Price, Url, Onsale, discount \n"
 out_filename = "SearchEngineData.csv"
 
 file = open(out_filename, "w", encoding='utf-8')
@@ -38,38 +38,39 @@ def beginScrape():
         # print(urlExt)
 
         # skip snow skaters
-        if catigoryName == "Snow Skate, Snow Surfers"  or catigoryName == "Snowboard Packages":
+        if catigoryName == "Snow Skate, Snow Surfers" or catigoryName == "Snowboard Packages":
             continue
         # if its a catigory that has sub classes go to the subcats function
-        if catigoryName == "Snowboarding Clothes & Apparel" or catigoryName == "Snowboard Accessories" or catigoryName =="Splitboard Gear":
+        if catigoryName == "Snowboarding Clothes & Apparel" or catigoryName == "Snowboard Accessories" or catigoryName == "Splitboard Gear":
             subCats(urlExt)
         else:
-            grabItems(urlExt,catigoryName)
+            grabItems(urlExt, catigoryName)
 
     file.close()
-#TODO! finish subcats
+
+
+# TODO! finish subcats
 # sub cats is basically thefirst for loop but needs to be done as some catigorys have more catigorys
 def subCats(extentionUrl):
     print("Sub Cats Entered")
-    subClient = uReq(regUrl+extentionUrl)
+    subClient = uReq(regUrl + extentionUrl)
     subsoup = bs(subClient.read(), "html.parser")
     subClient.close()
     time.sleep(5)
     cats = subsoup.findAll("div", {"class": "col-xs-6 skinny"})
     for cat in cats:
         catDetails = cat.find("div", {"class": "category-name"})
-        #print(catDetails)
+        # print(catDetails)
         catName = catDetails.strong.text
         Type = catName
 
-
-        #Clothing accessories has more categorys
+        # Clothing accessories has more categorys
         if catName == "Clothing Accessories":
             continue
-        preUrl =cat.find("a", {"class":"thumbnail text-center"})
-        postUrl = preUrl["href"].replace(",","")
+        preUrl = cat.find("a", {"class": "thumbnail text-center"})
+        postUrl = preUrl["href"].replace(",", "")
 
-        grabItems(postUrl,catName)
+        grabItems(postUrl, catName)
 
     return
 
@@ -81,96 +82,108 @@ def grabItems(extentionUrl, itemType):
     cagigorysoup = bs(newClient.read(), "html.parser")
     newClient.close()
     time.sleep(5)
-    Type = itemType
+    Type = itemType.replace(",", "")
     items = cagigorysoup.findAll("div", {"class": "col-sm-3 col-xs-6 col-xs-print-3 pull-left"})
-    grab(Type, items, regUrl+extentionUrl, cagigorysoup)
+    grab(Type, items, regUrl + extentionUrl, cagigorysoup)
     return
 
-def grab(Type, items,url, oldsoup ):
-    lastPage = False
-    count =1
+
+def grab(Type, items, url, oldsoup):
+    isLastPage = False
+    onlyOnePage = False
+    count = 1
     endofitems = oldsoup.findAll("a", {"aria-label": "Go to last page"})
+
     # print(endofitems)
     # print(endofitems[0]["href"])
     # print(url)
     # print(endofitems[0]["href"])
-    while lastPage != True:
-        if (url + "?nocache=1&refine.pagenumber=" + str(count)) == "https://www.The-House.com/" + endofitems[0]["href"]:
-            lastPage = True
+    if (url + "?nocache=1&refine.pagenumber=" + str(count)) == "https://www.The-House.com/" + endofitems[0]["href"]:
+        onlyOnePage = True
 
-        #print(count)
-        if count>1:
+    while (url + "?nocache=1&refine.pagenumber=" + str(count)) != "https://www.The-House.com/" + endofitems[0][
+        "href"] or isLastPage == True or onlyOnePage == True:
 
-            page ="?nocache=1&refine.pagenumber="+str(count)
-            newUrl = url+page
+        # print(count)
+        if count > 1:
+            page = "?nocache=1&refine.pagenumber=" + str(count)
+            newUrl = url + page
             newPageClient = uReq(newUrl)
             soup = bs(newPageClient.read(), "html.parser")
             newPageClient.close()
             time.sleep(5)
+
             items = soup.findAll("div", {"class": "col-sm-3 col-xs-6 col-xs-print-3 pull-left"})
         # print(len(items))
         # print(count)
         for item in items:
 
-
             onsale = "F"
             discount = ""
             name = item.div.div.a.img["alt"]
             picture = item.div.div.a.img["src"]
-            brandtitle = item.findAll("meta",{"itemprop": "brand"})
+            brandtitle = item.findAll("meta", {"itemprop": "brand"})
             # print(brandtitle[0]["content"])
             brand = brandtitle[0]["content"]
             # print(repr(brand))
             # print(repr(picture))
             # print(repr(name))
-            #year = ""
-            sizes =item.findAll("div",{"class": "category-product-title sizelist clearfix"})
+            # year = ""
+            sizes = item.findAll("div", {"class": "category-product-title sizelist clearfix"})
             # print(sizes)
             size = sizes[0].text
 
-            size = size.replace("Size:","").replace(",","-").replace("\n","").replace("\t","").strip()
+            size = size.replace("Size:", "").replace(",", "-").replace("\n", "").replace("\t", "").strip()
             # print(type(size))
             # print(repr(size))
-
 
             prePrice = item.findAll("span", {"itemprop": "price"})
             price = prePrice[0].text
             preurl = item.div.findAll("div", {"class": "category-product-title"})
             itemUrl = regUrl + preurl[0].a["href"]
-            #print(itemUrl)
-            pageClient= uReq(itemUrl)
+            # print(itemUrl)
+            pageClient = uReq(itemUrl)
             pagehtml = bs(pageClient.read(), "html.parser")
             time.sleep(5)
             pageClient.close()
-            #print(pagehtml)
+            # print(pagehtml)
             predesc = pagehtml.findAll("div", {"id": "product-description"})
-            if len(predesc) ==0:
-                itemDesc =""
+            if len(predesc) == 0:
+                itemDesc = ""
             else:
-                #print(predesc)
+                # print(predesc)
                 itemDesc = predesc[0].text
 
-
             # itemDesc= str(itemDesc).strip("\\t\\n")
-            itemDesc = itemDesc.replace(",","").replace("\n","").replace("\t","").replace("\v","").replace("\r","").replace("\b", "").strip()
+            itemDesc = itemDesc.replace(",", "").replace("\n", "").replace("\t", "").replace("\v", "").replace("\r",
+                                                                                                               "").replace(
+                "\b", "").strip()
             # print(itemDesc)
             # print(repr(itemDesc))
 
-            dis=item.findAll("span",{"class":"category-product-discount sale"})
+            dis = item.findAll("span", {"class": "category-product-discount sale"})
             # print(dis)
             if len(dis) != 0:
                 onsale = "T"
                 discount = str(dis[0].text)
-                discount = discount.replace("\t","").replace("\n","")
+                discount = discount.replace("\t", "").replace("\n", "")
                 # print(repr(discount))
             else:
-                discount=""
+                discount = ""
             # feilds = name,item desc, picture, type, brand,  year, size, price, url, is onsale, gender
             print(name)
-            #print(repr(str(name)+","+str(itemDesc) + ","+ str(picture)+ ","+ str(Type)+","+str(brand)+ ","+ str(size)+ ","+ str(price)+ ","+ str(itemUrl)+"," + onsale +"," + str(discount)))
-            file.write(str(name)+","+str(itemDesc) + ","+ str(picture)+ ","+ str(Type)+","+str(brand)+ ","+ str(size)+ ","+ str(price)+ ","+ str(itemUrl)+"," + str(onsale)+"," + str(discount)+ "\n" )
-        count +=1
+            # print(repr(str(name)+","+str(itemDesc) + ","+ str(picture)+ ","+ str(Type)+","+str(brand)+ ","+ str(size)+ ","+ str(price)+ ","+ str(itemUrl)+"," + onsale +"," + str(discount)))
+            file.write(
+                str(name) + "," + str(itemDesc) + "," + str(picture) + "," + str(Type) + "," + str(brand) + "," + str(
+                    size) + "," + str(price) + "," + str(itemUrl) + "," + str(onsale) + "," + str(discount) + "\n")
+        if isLastPage == False and onlyOnePage != True:
+            count += 1
+        if isLastPage == True:
+            isLastPage = False
+        elif (url + "?nocache=1&refine.pagenumber=" + str(count)) == "https://www.The-House.com/" + endofitems[0]["href"] and onlyOnePage != True:
+            isLastPage = True
 
+        onlyOnePage = False
 
 
 def main():
@@ -178,4 +191,3 @@ def main():
 
 
 main()
-
